b51f51d86eca0083750ef4b3af386ce2282f561c
Pawel Antkowiak <pwa@kmd.dk>
2023-10-22 21:30:29 +0200

    Initial commit

---
+++ src/main/java/com/kodilla/integration/FileTransformer.java
@@ -0,0 +1,25 @@
+package com.kodilla.integration;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * User: Z6PWA
+ * Date: 22.10.2023
+ */
+public class FileTransformer
+{
+  public String transformFile(String fileName) throws IOException
+  {
+    List<String> lines = Files.readAllLines(Paths.get(fileName));
+    StringBuilder result = new StringBuilder();
+
+    for (int n = lines.size() - 1; n >= 0; n--) {
+      result.append(lines.get(n) + "\n");
+    }
+
+    return result.toString();
+  }
+}
